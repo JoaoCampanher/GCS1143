f77d71603839e9b41e592c75e967edceace537b2
João Fonseca <joao.fonseca@altus.com.br>
2022-09-24 21:08:52 -0300

    separed the project in two files

---
+++ src/main/java/hello/Calculator.java
@@ -0,0 +1,11 @@
+package hello; 
+
+public class Calculator {
+    public static int getSecond(int n) {
+		return n * n;
+	}
+
+	public static int getThird(int n) {
+		return n * n * n;
+	}
+}
--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -4,23 +4,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		Scanner ent = new Scanner(System.in);
-		int n = ent.nextInt();
-		printValues(n);
-	}
+    public static void main(String[] args) throws IOException {
+        Calculator calculator = new Calculator();
+        Scanner ent = new Scanner(System.in);
+        int n = ent.nextInt();
 
-	public static void printValues(int n) {
-		for (int i = 1; i <= n; i++) {
-			System.out.println("" + i + " " + getSecond(i) + " " + getThird(i));
-		}
-	}
-
-	public static int getSecond(int n) {
-		return n * n;
-	}
-
-	public static int getThird(int n) {
-		return n * n * n;
-	}
+        for (int i = 1; i <= n; i++) {
+            System.out.println("" + i + " " + calculator.getSecond(i) + " " + calculator.getThird(i));
+        }
+    }
 }
